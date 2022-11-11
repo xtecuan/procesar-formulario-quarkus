3a8246811d6a010a127515dae4dd5ea467ec5d86
Tadeo Rivera-Pineda <xtecuan@protonmail.com>
2022-11-10 20:15:55 -0600

    Procesar la data de un formulario HTML

---
+++ src/main/java/sv/edu/ufg/ejemplos/procesar/formulario/resources/FormularioResource.java
@@ -0,0 +1,60 @@
+package sv.edu.ufg.ejemplos.procesar.formulario.resources;
+
+import org.jboss.resteasy.reactive.PartType;
+import org.jboss.resteasy.reactive.RestForm;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
+import sv.edu.ufg.ejemplos.procesar.formulario.utils.FileUtil;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import org.jboss.logging.Logger;
+
+@Path("procesar")
+public class FormularioResource {
+
+    /*public static class Person {
+        public String firstName;
+        public String lastName;
+        public String email;
+    }*/
+    private static final Logger LOG = Logger.getLogger(FormularioResource.class);
+
+    @POST
+    @Produces(MediaType.TEXT_HTML)
+    public Response multipart(
+            @RestForm("photo") FileUpload file,
+            @RestForm String firstName, @RestForm String lastName, @RestForm String email) {
+
+        if(file!=null){
+            try {
+                FileUtil.escribirImagen(file);
+            }catch (IOException e){
+                LOG.error("Error escribiendo el archivo: ",e);
+            }
+        }else {
+            LOG.error("No se subio el archivo");
+        }
+
+        StringBuilder sb = new StringBuilder("");
+        sb.append("<html>");
+        sb.append("<head><title>Reporte de Persona</title></head>");
+        sb.append("<body>");
+
+        sb.append("<ul>");
+        sb.append("<li>Nombre: " + firstName + "</li>");
+        sb.append("<li>Apellido: " + lastName + "</li>");
+        sb.append("<li>E-mail: " + email + "</li>");
+        sb.append("</ul>");
+
+        sb.append("</body>");
+        sb.append("</html>");
+        LOG.info("Se escribio el HTML");
+
+        return Response.ok(sb.toString(), MediaType.TEXT_HTML).build();
+    }
+
+}
---
+++ src/main/java/sv/edu/ufg/ejemplos/procesar/formulario/utils/FileUtil.java
@@ -0,0 +1,23 @@
+package sv.edu.ufg.ejemplos.procesar.formulario.utils;
+
+import org.apache.commons.io.FileUtils;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileUtil {
+
+    private static File folder = new File("C:\\UFG\\imagenes");
+
+    public static void escribirImagen(FileUpload file) throws IOException {
+        InputStream initialStream = FileUtils.openInputStream
+                (file.uploadedFile().toFile());
+        File targetFile = new File(folder,file.fileName());
+
+        FileUtils.writeByteArrayToFile(targetFile,initialStream.readAllBytes());
+
+
+    }
+}
